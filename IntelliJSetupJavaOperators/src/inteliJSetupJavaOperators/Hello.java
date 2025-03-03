b867ff21ef813eb3831f7f94b0273b312f323474
Aristóteles Pantaleão <62292967+AristotelesPantaleao@users.noreply.github.com>
2025-03-03 11:21:18 -0300

    Atualização 03/03/2025

--- IntelliJSetupJavaOperators/src/inteliJSetupJavaOperators/Hello.java
+++ IntelliJSetupJavaOperators/src/inteliJSetupJavaOperators/Hello.java
@@ -19,13 +19,56 @@ public class Hello {
             System.out.println("You got the high score!");
         }
 
+        int secondTopScore = 95;
 
-        int secondTopScore = 81;
-
-        if(topScore > secondTopScore && topScore < 100){
+        if((topScore > secondTopScore) && (topScore < 100)){
             System.out.println("Greater than second top score and less than 100");
         }
+
+        if((topScore > 90) || (secondTopScore <= 90)){
+            System.out.println("Either or both of the conditions are true");
+        }
         
+        int newValue = 50;
+
+        if(newValue == 50){
+            System.out.println("This is true");
+        }
+        
+        boolean isCard = false;
+
+        if(!isCard){
+            System.out.println("This is not supose to happen");
+        }
+        
+        String makeOfCard = "Volkswagen";
+
+        boolean isDomestic = makeOfCard == "Volkswagen" ? false : true;
+
+        if(isDomestic){
+            System.out.println("This car is domestic to our country");
+        }
+
+        String s = (isDomestic) ? "This car is domestic" : "This card is not domestic";
+
+        System.out.println(s);
+        
+        double myFirstValue = 20.00d;
+
+        double mySecondValue = 80.00d;
+
+        double myValuesTotal = ((myFirstValue + mySecondValue) * 100.00d);
+        System.out.println("MyValuesTotal = " + myValuesTotal);
+
+        double theRemainder = myValuesTotal % 40.00d;
+        System.out.println("theRemainder = " + theRemainder);
+
+        boolean isNoRemainder = theRemainder == 0 ? true : false;
+        System.out.println("isNoRemainder = " + isNoRemainder);
+
+        if(!isNoRemainder){
+            System.out.println("got some remainder");
+        }
 	}
 
 }
